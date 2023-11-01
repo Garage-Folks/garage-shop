e9a66759b8118f95422c42f3cd00226173efc586
Maximus Streeter <max.streeter10@gmail.com>
2023-10-31 21:56:52 -0400

    loginFunctionality: Initial custom identity provider and custom authentication state provider
    Signed-off-by: Maximus Streeter <max.streeter10@gmail.com>

---
+++ FineWoodworkingBasic/Authentication/Provider/UsersTable.cs
@@ -0,0 +1,77 @@
+﻿using FineWoodworkingBasic.Util;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace FineWoodworkingBasic.Authentication.Provider
+{
+    public class UsersTable
+    {
+        private readonly SqlConnection _connection;
+        public UsersTable(SqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public async Task<IdentityResult> CreateAsync(ApplicationUser user)
+        {
+            _connection.Open();
+
+            int rows = 0;
+
+            using (var command = new SqlCommand())
+            {
+                command.Connection = _connection;
+                command.CommandType = CommandType.Text;
+
+                SqlParameter parameter;
+
+                string sql = "INSERT INTO AuthorizedUser (Username, Password) " +
+                    " OUTPUT INSERTED.ID " +
+                    " VALUES (@Username, @Password);";
+                command.CommandText = sql;
+
+                parameter = new SqlParameter("@UserName", SqlDbType.NVarChar, 100);  // Fix Type and Length 
+                parameter.Value = user.UserName;
+                command.Parameters.Add(parameter);
+
+                parameter = new SqlParameter("@Password", SqlDbType.NVarChar, 100);  // Fix Type and Length 
+                parameter.Value = user.Password;
+                command.Parameters.Add(parameter);
+
+                rows = await command.ExecuteNonQueryAsync();
+            }
+
+            if (rows > 0)
+            {
+                return IdentityResult.Success;
+            }
+            return IdentityResult.Failed(new IdentityError { Description = $"Could not insert user {user.UserName}." });
+        }
+
+        public async Task<IdentityResult> DeleteAsync(ApplicationUser user)
+        {
+            string sql = "DELETE FROM dbo.CustomUser WHERE Id = @Id";
+            throw new NotImplementedException();
+        }
+
+        public async Task<ApplicationUser> FindByIdAsync(Guid userId)
+        {
+            string sql = "SELECT * " +
+                        "FROM dbo.CustomUsers " +
+                        "WHERE Id = @Id;";
+
+            throw new NotImplementedException();
+        }
+
+        public async Task<ApplicationUser> FindByNameAsync(string userName)
+        {
+            string sql = "SELECT * " +
+                        "FROM dbo.CustomUser " +
+                        "WHERE UserName = @UserName;";
+
+            throw new NotImplementedException();
+        }
+    }
+}
--- FineWoodworkingBasic/Program.cs
+++ FineWoodworkingBasic/Program.cs
@@ -3,10 +3,21 @@ using Microsoft.AspNetCore.Components.Web;
 using FineWoodworkingBasic.Util;
 using FineWoodworkingBasic.Service;
 using MudBlazor.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Components.Authorization;
+using FineWoodworkingBasic.Authentication.Provider;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using FineWoodworkingBasic.Provider;
 
 var builder = WebApplication.CreateBuilder(args);
 
+<<<<<<< HEAD
 Utilities.EstablishConnection("C:\\Users\\jaybo\\source\\repos\\garage-shop\\FineWoodworkingBasic\\DBConfig.ini");
+=======
+Utilities.EstablishConnection("C:\\Users\\nokil\\source\\repos\\garage-shop\\FineWoodworkingBasic\\dbConfig.ini");
+>>>>>>> 45aac51 (loginFunctionality: Initial custom identity provider and custom authentication state provider)
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -17,6 +28,29 @@ builder.Services.AddSingleton<AddBrandService>();
 // Add the AppState class
 builder.Services.AddScoped<AllStateInfoService>();
 builder.Services.AddMudServices();
+
+builder.Services.AddScoped<ProtectedSessionStorage>();
+
+// Add identity types
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 5;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.SignIn.RequireConfirmedEmail = false;
+
+});
+
+// Identity Services
+builder.Services.AddTransient<IUserStore<ApplicationUser>, CustomUserStore>();
+builder.Services.AddTransient<IRoleStore<ApplicationRole>, CustomRoleStore>();
+string connectionString = Utilities.GetConnectionString();
+builder.Services.AddTransient<SqlConnection>(e => new SqlConnection(connectionString));
+builder.Services.AddTransient<UsersTable>();
+
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 
 var app = builder.Build();
 
@@ -30,6 +64,8 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
