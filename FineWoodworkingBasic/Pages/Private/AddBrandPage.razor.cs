e35176a9d9cd08940c3aa1895efc1abc94817791
Maximus Streeter <max.streeter10@gmail.com>
2023-12-14 14:32:39 -0500

    feature: Added add brand feature
    Signed-off-by: Maximus Streeter <max.streeter10@gmail.com>

---
+++ FineWoodworkingBasic/Pages/Private/AddBrandPage.razor.cs
@@ -0,0 +1,32 @@
+﻿using FineWoodworkingBasic.Model;
+using Microsoft.AspNetCore.Components;
+
+namespace FineWoodworkingBasic.Pages.Private
+{
+    partial class AddBrandPage : ComponentBase
+    {
+        private string addBrandMessage { get; set; }
+        private string bName { get; set; }
+        private string bNotes { get; set; }
+
+        public string CssClass { get; set; }
+
+        private void SetBName(ChangeEventArgs changeEvent)
+        {
+            bName = (string)changeEvent.Value;
+        }
+
+        private void SetBNotes(ChangeEventArgs changeEvent)
+        {
+            bNotes = (string)changeEvent.Value;
+        }
+
+        private async void AddBrandAction()
+        {
+
+
+            ResultMessage mesg = await svc.AddBrandAsync(bName, bNotes);
+            addBrandMessage = mesg.Message;
+        }
+    }
+}
