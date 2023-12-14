629dd808852c081f7aa41d4eaa70ae2b51a01b2f
Dustin Meyer <60855358+LostProgrammer1010@users.noreply.github.com>
2023-12-14 13:49:43 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using FineWoodworkingBasic.Util;
+using FineWoodworkingBasic.Service;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Utilities.EstablishConnection("C:\\Users\\emr19\\source\\repos\\garage-shop\\FineWoodworkingBasic\\dbConfig.ini");
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+// Register the login service
+builder.Services.AddSingleton<LoginService>();
+builder.Services.AddSingleton<AddBrandService>();
+// Add the AppState class
+builder.Services.AddScoped<AllStateInfoService>();
+builder.Services.AddMudServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
