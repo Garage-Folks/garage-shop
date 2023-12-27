9aabf009aaa300c1cdd01a60abd06caadd4fff76
DuyMach <108498281+DuyMach@users.noreply.github.com>
2023-12-27 18:35:08 -0500

    Updated Oil class and added OilCollection

---
+++ FineWoodworkingBasic/Model/Oil.cs
@@ -0,0 +1,254 @@
+﻿using System.Data.SqlTypes;
+using QC = Microsoft.Data.SqlClient;
+using DT = System.Data;
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace FineWoodworkingBasic.Model
+{
+    public class Oil : InventoryItem
+    {
+        protected string Nature { get; set; }
+
+        // Foreign Key
+        protected SqlGuid BrandID { get; set; }
+
+
+
+        public Oil(SqlGuid id, string name, string notes, string fileImg1, string fileImg2,
+            string fileImg3, int quantity, string nature, SqlGuid brandID) : 
+            base(id, name, notes, fileImg1, fileImg2, fileImg3, quantity)
+        {
+            SetNature(nature);
+            BrandID = brandID;
+        }
+        
+        public Oil(string name, string notes, string fileImg1, string fileImg2,
+            string fileImg3, int quantity, string nature, SqlGuid brandID) :
+            base(name, notes, fileImg1, fileImg2, fileImg3, quantity)
+        {
+            SetNature(nature);
+            BrandID = brandID;
+        }
+
+        private void SetNature(string nature)
+        {
+            string uppercaseNature = nature.ToUpper();
+
+            if (uppercaseNature == "DRYING" || uppercaseNature == "NON-DRYING")
+            {
+                Nature = uppercaseNature;
+            } else
+            {
+                throw new ArgumentException("Invalid value for Nature. Must be 'Drying' or 'Non-Drying'.");
+            }
+        }
+
+        public override bool IsPopulated()
+        {
+            if (this.ID == null) return false;
+            if (this.ID.Equals(0)) return false;
+            return true;
+        }
+
+        protected override void ConstructPopulateQueryCommand(Dictionary<string, object> dictIdToUse, QC.SqlCommand command)
+        {
+            QC.SqlParameter parameter;
+            String query = @"SELECT * FROM Oil WHERE (ID = @NP);";
+
+            command.CommandText = query;
+            parameter = new SqlParameter("@NP", DT.SqlDbType.UniqueIdentifier);
+            parameter.Value = dictIdToUse["id"];
+            command.Parameters.Add(parameter);
+        }
+
+        protected override ResultMessage GetErrorMessageForDelete(Exception excep)
+        {
+            ResultMessage mesg = new ResultMessage(ResultMessage.ResultMessageType.Error, "Error in deleting Oil with Name: " + this.Name
+                    + " from the database!");
+            return mesg;
+        }
+
+        protected override ResultMessage GetErrorMessageForPopulate(Exception excep)
+        {
+            ResultMessage mesg = new ResultMessage(ResultMessage.ResultMessageType.Error, "Error in retrieving Oil with ID: " + this.ID
+                    + " from the database!");
+            return mesg;
+        }
+
+        protected override ResultMessage GetErrorMessageForSave(Exception excep)
+        {
+            ResultMessage mesg = new ResultMessage(ResultMessage.ResultMessageType.Error, "Error in saving Oil with Name: " + this.Name
+                    + " into the database!");
+            return mesg;
+        }
+
+        protected override ResultMessage GetResultMessageForDelete()
+        {
+            ResultMessage mesg = new ResultMessage(ResultMessage.ResultMessageType.Success, "Oil with Name: " + this.Name
+                    + " deleted successfully from the database!");
+            return mesg;
+        }
+
+        protected override ResultMessage GetResultMessageForPopulate()
+        {
+            ResultMessage mesg = new ResultMessage(ResultMessage.ResultMessageType.Success, "Oil with ID: " + this.ID
+                    + " retrieved successfully from the database!");
+            return mesg;
+        }
+
+        protected override ResultMessage GetResultMessageForSave()
+        {
+            ResultMessage mesg = new ResultMessage(ResultMessage.ResultMessageType.Success, "Oil with Name: " + this.Name
+                    + " saved successfully into the database!");
+            return mesg;
+        }
+
+        protected override bool IsNewObject()
+        {
+            if (ID == null)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        protected override void ProcessPopulateQueryResult(QC.SqlDataReader reader)
+        {
+            while (reader.Read())
+            {
+                ID = reader.GetSqlGuid(reader.GetOrdinal("ID"));
+                LocationID = reader.GetSqlGuid(reader.GetOrdinal("LocationID"));
+                BrandID = reader.GetSqlGuid(reader.GetOrdinal("BrandID"));
+                Name = reader.GetString(reader.GetOrdinal("Name"));
+                Nature = reader.GetString(reader.GetOrdinal("Nature"));
+                FileImage1 = reader.GetString(reader.GetOrdinal("LinkImg1"));
+                FileImage2 = reader.GetString(reader.GetOrdinal("LinkImg2"));
+                FileImage3 = reader.GetString(reader.GetOrdinal("LinkImg3"));
+                Quantity = reader.GetInt32(reader.GetOrdinal("Qty"));
+                Notes = reader.GetString(reader.GetOrdinal("Notes"));
+            }
+        }
+
+        protected override void SetupCommandForDelete(QC.SqlCommand command)
+        {
+            QC.SqlParameter parameter;
+
+            string deleteQuery = "DELETE FROM Oil " +
+                " (WHERE ID = @ID)";
+
+            command.CommandText = deleteQuery;
+
+            parameter = new QC.SqlParameter("@ID", DT.SqlDbType.UniqueIdentifier);  // Fix Type and Length 
+            parameter.Value = this.ID;
+            command.Parameters.Add(parameter);
+        }
+
+        protected override void SetupCommandForInsert(QC.SqlCommand command)
+        {
+            // Taking a 'PreparedStatement' approach here, avoids SQL Injection  
+            // THIS IS IMPORTANT 
+
+            QC.SqlParameter parameter;
+
+            string insertQuery = "INSERT INTO Oil (Name, Notes, LinkImg1, LinkImg2, LinkImg3, Qty, LocationID, Nature, BrandedID) " +
+                " OUTPUT INSERTED.ID " +
+                " VALUES (@Name, @Notes, @LinkImg1, @LinkImg2, @LinkImg3, @Qty, @LocationID, @Nature, @BrandID);";
+
+            command.CommandText = insertQuery;
+
+            parameter = new QC.SqlParameter("@Name", DT.SqlDbType.NVarChar, 100);  // Fix Type and Length 
+            parameter.Value = Name;
+            command.Parameters.Add(parameter);
+
+            parameter = new QC.SqlParameter("@Notes", DT.SqlDbType.NVarChar, 1000); // Fix Type and Length  
+            parameter.Value = Notes;
+            command.Parameters.Add(parameter);
+
+            parameter = new QC.SqlParameter("@LinkImg1", DT.SqlDbType.NVarChar, 1000); // Fix Type and Length  
+            parameter.Value = FileImage1;
+            command.Parameters.Add(parameter);
+
+            parameter = new QC.SqlParameter("@LinkImg2", DT.SqlDbType.NVarChar, 1000); // Fix Type and Length  
+            parameter.Value = FileImage2;
+            command.Parameters.Add(parameter);
+
+            parameter = new QC.SqlParameter("@LinkImg3", DT.SqlDbType.NVarChar, 1000); // Fix Type and Length  
+            parameter.Value = FileImage3;
+            command.Parameters.Add(parameter);
+
+            parameter = new QC.SqlParameter("@Qty", DT.SqlDbType.Int, 1000); // Fix Type and Length  
+            parameter.Value = Quantity;
+            command.Parameters.Add(parameter);
+
+            parameter = new QC.SqlParameter("@LocationID", DT.SqlDbType.UniqueIdentifier, 1000); // Fix Type and Length  
+            parameter.Value = LocationID;
+            command.Parameters.Add(parameter);
+
+            parameter = new QC.SqlParameter("@BrandID", DT.SqlDbType.UniqueIdentifier, 1000); // Fix Type and Length  
+            parameter.Value = BrandID;
+            command.Parameters.Add(parameter);
+        }
+
+        protected override void SetAutogeneratedIDFromInsert(SqlGuid genID)
+        {
+            this.ID = genID;
+        }
+
+        protected override void SetupCommandForUpdate(QC.SqlCommand command)
+        {
+            QC.SqlParameter parameter;
+
+            string updateQuery = "UPDATE Oil" +
+               " SET Name = @Name, Notes = @Notes, LinkImg1 = @LinkImg1, " +
+               " LinkImg2 = @LinkImg2, LinkImg3 = @LinkImg3, Qty = @Qty, LocationID = @LocationID," +
+               " Nature = @Nature, BrandID = @BrandID " +
+               " WHERE (ID = @Id);";
+
+            command.CommandText = updateQuery;
+
+            parameter = new QC.SqlParameter("@Name", DT.SqlDbType.NVarChar, 100);  // Fix Type and Length 
+            parameter.Value = Name;
+            command.Parameters.Add(parameter);
+
+            parameter = new QC.SqlParameter("@Notes", DT.SqlDbType.NVarChar, 1000); // Fix Type and Length  
+            parameter.Value = Notes;
+            command.Parameters.Add(parameter);
+
+            parameter = new QC.SqlParameter("@LinkImg1", DT.SqlDbType.NVarChar, 1000); // Fix Type and Length  
+            parameter.Value = FileImage1;
+            command.Parameters.Add(parameter);
+
+            parameter = new QC.SqlParameter("@LinkImg2", DT.SqlDbType.NVarChar, 1000); // Fix Type and Length  
+            parameter.Value = FileImage2;
+            command.Parameters.Add(parameter);
+
+            parameter = new QC.SqlParameter("@LinkImg3", DT.SqlDbType.NVarChar, 1000); // Fix Type and Length  
+            parameter.Value = FileImage3;
+            command.Parameters.Add(parameter);
+
+            parameter = new QC.SqlParameter("@Qty", DT.SqlDbType.Int, 1000); // Fix Type and Length  
+            parameter.Value = Quantity;
+            command.Parameters.Add(parameter);
+
+            parameter = new QC.SqlParameter("@LocationID", DT.SqlDbType.UniqueIdentifier, 1000); // Fix Type and Length  
+            parameter.Value = LocationID;
+            command.Parameters.Add(parameter);
+
+            parameter = new QC.SqlParameter("@Nature", DT.SqlDbType.NVarChar, 1000); // Fix Type and Length  
+            parameter.Value = Nature;
+            command.Parameters.Add(parameter);
+
+            parameter = new QC.SqlParameter("@BrandID", DT.SqlDbType.UniqueIdentifier, 1000); // Fix Type and Length  
+            parameter.Value = BrandID;
+            command.Parameters.Add(parameter);
+
+            parameter = new QC.SqlParameter("@Id", DT.SqlDbType.UniqueIdentifier);  // Fix Type and Length 
+            parameter.Value = ID;
+            command.Parameters.Add(parameter);
+        }
+
+    }
+}
---
+++ FineWoodworkingBasic/Model/OilCollection.cs
@@ -0,0 +1,223 @@
+﻿using System.Text;
+using DT = System.Data;
+using QC = Microsoft.Data.SqlClient;
+using FineWoodworkingBasic.Util;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Linq.Expressions;
+using System.Data.SqlTypes;
+
+namespace FineWoodworkingBasic.Model
+{
+    public class OilCollection : PersistableCollection
+    {
+        protected List<Oil> OilList;
+
+        protected delegate void PopulateQueryMethodType(Dictionary<string, Object> val, QC.SqlCommand command);
+
+        protected PopulateQueryMethodType QueryMethod;
+
+        public OilCollection()
+        {
+            OilList = new List<Oil>();
+            QueryMethod = QueryConstructorAll;
+        }
+
+        // Fully helper methods
+        protected override void ProcessPopulateQueryResult(QC.SqlDataReader reader)
+        {
+            while (reader.Read())
+            {
+                SqlGuid ID = reader.GetSqlGuid(reader.GetOrdinal("ID"));
+                string Name = reader.GetString(reader.GetOrdinal("Name"));
+                string Notes = reader.GetString(reader.GetOrdinal("Notes"));
+                string FileImage1 = reader.GetString(reader.GetOrdinal("LinkImg1"));
+                string FileImage2 = reader.GetString(reader.GetOrdinal("LinkImg2"));
+                string FileImage3 = reader.GetString(reader.GetOrdinal("LinkImg3"));
+                int Quantity = reader.GetInt32(reader.GetOrdinal("Qty"));
+                SqlGuid LocationID = reader.GetSqlGuid(reader.GetOrdinal("LocationID"));
+                string Nature = reader.GetString(reader.GetOrdinal("Nature"));
+                SqlGuid BrandID = reader.GetSqlGuid(reader.GetOrdinal("BrandID"));
+                Oil oil = new Oil(ID, Name, Notes, FileImage1, FileImage2, FileImage3, Quantity, Nature, BrandID);
+                oil.SetLocationID(LocationID);
+                OilList.Add(oil);
+
+            }
+        }
+
+        public void PopulateAll()
+        {
+            QueryMethod = new PopulateQueryMethodType(QueryConstructorAll);
+            Dictionary<string, Object> d = new Dictionary<string, Object>();
+            PopulateHelper(d);
+        }
+
+        public void PopulateViaName(string name)
+        {
+            QueryMethod = new PopulateQueryMethodType(QueryConstructorViaName);
+            Dictionary<string, Object> d = new Dictionary<string, Object>();
+            d["name"] = name;
+            PopulateHelper(d);
+        }
+
+        public void PopulateViaBrandName(string brandName)
+        {
+            QueryMethod = new PopulateQueryMethodType(QueryConstructorViaBrandName);
+            Dictionary<string, Object> d = new Dictionary<string, Object>();
+            d["brandName"] = brandName;
+            PopulateHelper(d);
+        }
+
+        public void PopulateViaBrandID(SqlGuid brandID)
+        {
+            QueryMethod = new PopulateQueryMethodType(QueryConstructorViaBrandID);
+            Dictionary<string, Object> d = new Dictionary<string, Object>();
+            d["brandID"] = brandID;
+            PopulateHelper(d);
+        }
+
+        public void PopulateViaNature(string nature)
+        {
+            QueryMethod = new PopulateQueryMethodType(QueryConstructorViaNature);
+            Dictionary<string, Object> d = new Dictionary<string, Object>();
+            d["oilNature"] = nature;
+            PopulateHelper(d);
+        }
+
+        public void PopulateViaBrandNameAndNature(string brandName, string nature)
+        {
+            QueryMethod = new PopulateQueryMethodType(QueryConstructorViaBrandNameAndNature);
+            Dictionary<string, Object> d = new Dictionary<string, Object>();
+            d["brandName"] = brandName;
+            d["oilNature"] = nature;
+            PopulateHelper(d);
+        }
+
+        protected override void ConstructPopulateQueryCommand(Dictionary<string, Object> val, QC.SqlCommand command)
+        {
+            QueryMethod(val, command);
+        }
+
+        protected virtual void QueryConstructorAll(Dictionary<string, Object> dictNamePart, QC.SqlCommand command)
+        {
+            string query = @"SELECT * FROM Oil";
+
+            command.CommandText = query;
+        }
+
+        protected virtual void QueryConstructorViaName(Dictionary<string, Object> dictNotesPart, QC.SqlCommand command)
+        {
+            QC.SqlParameter parameter;
+
+            string query = @"SELECT * FROM Oil WHERE (Name LIKE CONCAT('%', @NP, '%'));";
+
+            command.CommandText = query;
+
+            parameter = new QC.SqlParameter("@NP", DT.SqlDbType.NVarChar, 1000);  // Fix Type and Length 
+            parameter.Value = dictNotesPart["name"];
+            command.Parameters.Add(parameter);
+        }
+
+        protected virtual void QueryConstructorViaBrandName(Dictionary<string, Object> dictNotesPart, QC.SqlCommand command)
+        {
+            QC.SqlParameter parameter;
+
+            string query = @"SELECT * FROM Oil INNER JOIN Brand ON
+                            (Oil.BrandID = Brand.ID)
+                            AND (Brand.Name LIKE CONCAT('%', @BRAND, '%'));";
+
+            command.CommandText = query;
+
+            parameter = new QC.SqlParameter("@BRAND", DT.SqlDbType.NVarChar, 1000);  // Fix Type and Length 
+            parameter.Value = dictNotesPart["brandName"];
+            command.Parameters.Add(parameter);
+        }
+
+        protected virtual void QueryConstructorViaBrandID(Dictionary<string, Object> dictNotesPart, QC.SqlCommand command)
+        {
+            QC.SqlParameter parameter;
+
+            string query = @"SELECT * FROM Oil WHERE BrandID = @BRANDID;";
+
+            command.CommandText = query;
+
+            parameter = new QC.SqlParameter("@BRANDID", DT.SqlDbType.UniqueIdentifier, 1000);  // Fix Type and Length 
+            parameter.Value = dictNotesPart["brandID"];
+            command.Parameters.Add(parameter);
+        }
+
+        protected virtual void QueryConstructorViaNature(Dictionary<string, Object> dictNotesPart, QC.SqlCommand command) 
+        {
+            QC.SqlParameter parameter;
+
+            string query = @"SELECT * FROM Oil WHERE (Nature LIKE CONCAT('%', @NATURE, '%'));";
+            command.CommandText = query;
+
+            parameter = new QC.SqlParameter("@NATURE", DT.SqlDbType.NVarChar, 1000);
+            parameter.Value = dictNotesPart["oilNature"];
+            command.Parameters.Add(parameter);
+        }
+
+        protected virtual void QueryConstructorViaBrandNameAndNature(Dictionary<string, Object> dictNotesPart, QC.SqlCommand command)
+        {
+            QC.SqlParameter parameter;
+
+            string query = @"SELECT * FROM Oil INNER JOIN Brand ON
+                            (Oil.BrandID = Brand.ID)
+                            AND (Brand.Name LIKE CONCAT('%', @BRAND, '%'))
+                            AND (Oil.Nature LIKE CONCAT('%', @NATURE, '%'));";
+
+            command.CommandText = query;
+
+            parameter = new QC.SqlParameter("@BRAND", DT.SqlDbType.NVarChar, 1000);  // Fix Type and Length 
+            parameter.Value = dictNotesPart["brandName"];
+            command.Parameters.Add(parameter);
+
+            parameter = new QC.SqlParameter("@NATURE", DT.SqlDbType.NVarChar, 1000);  // Fix Type and Length 
+            parameter.Value = dictNotesPart["oilNature"];
+            command.Parameters.Add(parameter);
+        }
+
+        protected override ResultMessage GetResultMessageForPopulate()
+        {
+            ResultMessage mesg = new ResultMessage(ResultMessage.ResultMessageType.Success, "Oil Collection " +
+                " retrieved successfully!");
+            return mesg;
+        }
+
+        protected override ResultMessage GetResultMessageForSave()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override ResultMessage GetErrorMessageForPopulate(Exception Ex)
+        {
+            ResultMessage mesg = new ResultMessage(ResultMessage.ResultMessageType.Error, "Error in retrieving Oil Collection " +
+                " from database!");
+            return mesg;
+        }
+
+        protected override ResultMessage GetErrorMessageForSave(Exception Ex)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override string ToString()
+        {
+            string retVal = "";
+            for (int cnt = 0; cnt < OilList.Count; cnt++)
+            {
+                retVal += OilList[cnt].ToString();
+            }
+
+            return retVal;
+        }
+
+
+    }
+
+
+
+
+
+}
