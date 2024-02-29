dd87756d4ca98fdb5952e1429f96b7f85d9cba28
EricRosenhein <128406706+EricRosenhein@users.noreply.github.com>
2024-02-28 22:23:36 -0500

    Templates for UI

--- FineWoodworkingBasic/Pages/Private/AddBrandPage.razor.cs
+++ FineWoodworkingBasic/Pages/Private/AddBrandPage.razor.cs
@@ -2,19 +2,19 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using System.ComponentModel.DataAnnotations;
-
-
+using MudBlazor;
 
 namespace FineWoodworkingBasic.Pages.Private
 {
+    #region AddBrandPage Class
     partial class AddBrandPage : ComponentBase
     {
-        bool success;
-        bool spin;
-        AddBrandForm model = new AddBrandForm(); 
+        // Parameter with helper variables to maintain a modified and unmodified state of the form.
+        public AddBrandForm model = new AddBrandForm();
         private string addBrandMessage { get; set; }
-        private string bName { get; set; }
-        private string bNotes { get; set; }
+
+        Color msgColor = Color.Error;
+        bool spin;
 
         /// <summary>
         /// Basic form validation passed, checking for existing entries and returning message. 
@@ -22,13 +22,33 @@ namespace FineWoodworkingBasic.Pages.Private
         /// <param name="context"></param>
         private async void OnValidSubmit(EditContext context)
         {
-            success = true;
+            spin = true;
 
             ResultMessage mesg = await svc.AddBrandAsync(model.Name, model.Notes);
+
             addBrandMessage = mesg.Message;
+            string _compare = "success";
+            if (addBrandMessage.Contains(_compare))
+            {
+                msgColor = Color.Success;
+            }
+
+            spin = false;
             StateHasChanged();
         }
 
+        /// <summary>
+        /// Returns all fields to empty and resets the status message.
+        /// </summary>
+        private void ClearAll()
+        {
+            model.Name = string.Empty;
+            model.Notes = string.Empty;
+            addBrandMessage = string.Empty;
+            StateHasChanged();
+        }
+
+        #region Error Messages (Validation Tag)
         private IEnumerable<string> MaxFiftyCharacters(string ch)
         { 
             if (!string.IsNullOrEmpty(ch) && 50 < ch?.Length)
@@ -40,7 +60,12 @@ namespace FineWoodworkingBasic.Pages.Private
             if (!string.IsNullOrEmpty(ch) && 2000 < ch?.Length)
                 yield return "Max 2000 characters";
         }
+        #endregion
+
     }
+    #endregion
+
+    #region AddBrandForm Class
     public class AddBrandForm
     {
         [Required]
@@ -51,5 +76,5 @@ namespace FineWoodworkingBasic.Pages.Private
         [StringLength(2000, ErrorMessage = "Notes length can't be greater than 2000 characters.")]
         public string Notes { get; set; }
     }
-
+    #endregion
 }
